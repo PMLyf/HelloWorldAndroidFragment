b4dec5cd4b6ce3c88894aaf09ba1f39c621b3fb3
PMLyf <david.irvinge@gmail.com>
2022-04-27 18:51:24 -0400

    This is a continuation of  the previous example activity  * Purpose - Display a textView from a Fragment.  * Notations will not include what was previously covered in example activity  *  Step 1 -  Create a Main activity.

---
+++ app/src/main/java/com/nightlyexamples/fragmentexample/Main.kt
@@ -0,0 +1,17 @@
+package com.nightlyexamples.fragmentexample
+
+class Main {
+
+}
+
+
+
+/**
+ *
+ * This is a continuation of  the previous example activity
+ * Purpose - Display a textView from a Fragment.
+ * Notations will not include what was previously covered in example activity
+ *
+ *  Step 1 -  Create a Main activity.
+ *
+ * */
